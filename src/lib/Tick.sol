1bb87fefbb651f02e3f72784ae5f04f3a7b782a6
frank hu <frankhu@franks-MacBook-Air.local>
2023-10-01 23:21:50 +0800

    part 0&1 tick spacing

---
+++ src/lib/Tick.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+library Tick {
+    function tickSpacingToMaxLiquidityPerTick(int24 tickSpacing) internal pure returns(uint128){
+        //TODO: right code
+    }
+}

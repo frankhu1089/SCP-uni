e7d33af491830791be1846bb21bd38a5d2ecad46
Frank Hu <f55665566@gmail.com>
2023-10-02 17:05:47 +0800

    part 3: initialized and slot0 struct

--- src/CLAMM.sol
+++ src/CLAMM.sol
@@ -3,12 +3,23 @@ pragma solidity ^0.8.19;
 import "./lib/Tick.sol";
 
 contract CLAMM {
+    
     address public immutable token0;
     address public immutable token1;
     uint24 public immutable fee;
     int24 public immutable tickSpacing;
     uint128 public immutable maxLiquidityPerTick;
     // struct Slot0{};
+    struct Slot0 {
+        // the current price
+        uint160 sqrtPriceX96;
+        // the current tick
+        int24 tick;
+        // whether the pool is locked
+        bool unlocked;
+    }
+    Slot0 public slot0;
+    
     constructor(
         address _token0,
         address _token1,
@@ -22,5 +33,15 @@ contract CLAMM {
 
         maxLiquidityPerTick = Tick.tickSpacingToMaxLiquidityPerTick(tickSpacing);
 
+    }
+    function initialize(uint160 sqrtPriceX96) external {
+        require(slot0.sqrtPriceX96 == 0, 'already initializzed');
+        int24 tick = TickMath.getTickAtSqrtRatio(sqrtPriceX96);
+
+        slot0 = Slot0({
+            sqrtPriceX96: sqrtPriceX96,
+            tick: tick,
+            unlocked: true
+        });
     }
 }
